34521b1bb2b93643b14518ee0ca88c289aff6677
Animeaboutmath <Clements131@gmail.com>
2019-06-25 04:10:30 -0400

    header file to read input information
    file for reading input of text for bastard bot, accepts file-name and read and write

---
+++ file_input.h
@@ -0,0 +1,22 @@
+class file_input(input_text, filename)
+{
+  /*
+  I'm putting this code here because I just want to bitch because evangelion is
+  kind of a shit anime, I'm not impressed :^0
+  */
+  ofstream filename
+public:
+  void readFile(filename)
+  {
+  filename.open ("example.txt");
+  filename << "Writing this to a file.\n";
+  filename.close();
+  }
+
+  void writeFile()
+  {
+  filename.open ("example.txt");
+  filename << "Writing this to a file.\n";
+  filename.close();
+  }
+};
